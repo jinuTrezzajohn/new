16ff55714fab5e35d83340742a19ba356a3d3cd7
jinutrezzajohn6 <jinujohn685@gmail.com>
2023-08-01 18:08:37 +0530

    dictionary of cube

--- fogg.py
+++ fogg.py
@@ -1,6 +1,7 @@
 l1=[5,6,1,2,3]
 l2=['abc','def','xyz']
 d={}
+d1={}
 for i in l1:
     print(i**2)
 for i in l1:
@@ -8,5 +9,8 @@ for i in l1:
 for i in range(len(l1)):
     d[i]=(l1[i]**2)
 print(d)
+for i in range(len(l1)):
+    d1[i]=(l1[i]**3)
+print(d1)
 
 
