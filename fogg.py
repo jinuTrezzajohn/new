bcad1e04cabdc1197d22dba01b8fe63413d34473
jinutrezzajohn6 <jinujohn685@gmail.com>
2023-08-01 17:55:52 +0530

    counted vowels

--- fogg.py
+++ fogg.py
@@ -2,3 +2,11 @@ l1=[5,6,1,2,3]
 l2=['abc','def','xyz']
 for i in l2:
     print(len(i))
+c=0
+for i in l2:
+    c=0
+    for j in range(len(i)):
+        if i[j] in ['a','e','i','o','u']:
+            c+=1
+    print(c)
+            
